1a01ad2dd0b6a80209b8002cda7c2eab7d0be343
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-10-02 22:57:13 -0400

    changed up the color scheme for the website and worked on styling the nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import About from './components/About';
+import Banner from './components/Banner';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Services from './components/Services';
+import Work from './components/Work';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div className="text-black p-4 rounded-none  bg-site bg-no-repeat bg-cover overflow-hidden h-[4000px]">
+        <Header />
+        <Banner />
+        <Nav />
+        <About />
+        <Services />
+        <Work />
+        <Contact />
+        <div classname=""></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Rajdhani',
+      tertiary: 'Aldrich',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent: '#8B4513',
+      },
+      letterSpacing: {
+        '10%': '0.1em',
+      },
+      borderRadius: {
+        full: '100%',
+      },
+      boxShadow: {
+        'btn': '0px 4.42184px 107.23px rgba(139, 69, 19, 0.51)',
+      },
+      backgroundImage: {
+        site: "url('./assets/new-bg.jpeg')",
+        about: "url('./assets/headshot.png)",
+        services: "url('./assets/headshot.png)",
+        'btn-gradient': 'linear-gradient(92.23deg, #d2691e 21.43%, #8b4513 50.63%, #a0522d 100%, #8b0000 117.04%)',
+        'btn-gradient-hover': 'linear-gradient(92.23deg, #8b0000 21.43%, #a0522d 50.63%, #8b4513 100%, #d2691e 117.04%)',
+      },
+    },
   },
   plugins: [],
 }
