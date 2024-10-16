352f49e86add136298cc4f9a805fda55d574de3a
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-10-16 12:10:41 -0400

    removed animation from name in banner

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,85 @@
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
-    return (
-        <div className="py-8 lg:py-24 lg:h-screen flex items-center" id='about'>About</div>
-    )
-}
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
+  return (
+    <div
+      className="py-8 lg:py-24 lg:h-screen flex items-center"
+      id="about"
+      ref={ref}
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          {/* {img} */}
+          <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px]
+            mix-blend-lighten bg-top"
+          ></motion.div>
+          {/* {text} */}
+          <motion.div 
+          variants={fadeIn('left', 0.5)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3 }}
+          className="flex-1">
+            <h2 className="h2 text-accent">About me</h2>
+            <h3 className="h3 mb-4">
+              I'm a Freelance Front-end Developer with 2 years of experience.
+            </h3>
+            <p className="mb-6">
+              Do laboris mollit labore minim ipsum. Nisi incididunt veniam ipsum
+              irure veniam reprehenderit dolor consectetur id voluptate. Laboris
+              officia excepteur ipsum consectetur velit aliquip ut pariatur.
+              Nulla ea commodo adipisicing consequat mollit sint ut est do culpa
+              ipsum velit. Do esse eu dolor laboris pariatur dolore eu cupidatat
+              eiusmod magna ea dolore aliquip nisi. Laboris do aute eiusmod
+              nulla mollit dolor reprehenderit exercitation ullamco enim.
+            </p>
+            {/* {stats} */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={2} duration={2}/> : null
+                  }
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br/>
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={5} duration={3}/> : null
+                  }
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br/>
+                  Completed
+                </div>
+              </div>
+            </div>
+            <div>
+              <button className="btn btn-lg"> Contact Me</button>
+              <a href='#' className="text-gradient btn-link">
+                My Portfolio
+                </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,12 +7,13 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section
-      className="section py-8 lg:py-24 lg:h-screen flex items-center min-h-[85vh] lg:min-h-[78vh]"
+    <div
+      className="section flex items-center min-h-[85vh] lg:min-h-[78vh]"
       id="home"
     >
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          {/* {text} */}
           <div className="flex-1 text-center font-secondary lg:text-left">
             <motion.h1
               variants={fadeIn("up", 0.3)}
@@ -45,41 +46,62 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-              <p className="mb-8 max-w-lg mx-auto lg:mx-0 ">
-                Incididunt adipisicing in ullamco officia reprehenderit duis do
-                ut est eu nulla.{" "}
-              </p>
-              <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
-                <button className="bg-btn-gradient hover:bg-btn-gradient-hover rounded-3xl font-primary text-white font-medium h-[56px] px-10 text-base">
-                  Contact me
-                </button>
-                <a
-                  href="#"
-                  className="bg-gradient-to-r text-transparent bg-clip-text from-[#d8cbc4] to-[#765341] font-bold font-primary hover:from-[#765341] hover:to-[#d8cbc4] text-base"
-                >
-                  My Portfolio{" "}
-                </a>
-              </div>
-              <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+            </motion.div>
+            <motion.p 
+            variants={fadeIn("up", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="mb-8 max-w-lg mx-auto lg:mx-0 ">
+              Incididunt adipisicing in ullamco officia reprehenderit duis do ut
+              est eu nulla.{" "}
+            </motion.p>
+            <motion.div 
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="bg-btn-gradient hover:bg-btn-gradient-hover rounded-3xl font-primary text-white font-medium h-[56px] px-10 text-base">
+                Contact me
+              </button>
+              <a
+                href="#"
+                className="bg-gradient-to-r text-transparent bg-clip-text from-[#d8cbc4] to-[#765341] font-bold font-primary hover:from-[#765341] hover:to-[#d8cbc4] text-base"
+              >
+                My Portfolio{" "}
+              </a>
+            </motion.div>
+            {/* {socials} */}
+            <motion.div 
+            variants={fadeIn("up", 0.7)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
                 <a href="#">
                   <FaYoutube />
                 </a>
                 <a href="#">
                   <FaGithub />
                 </a>
-              </div>
-            </motion.div>
+              </motion.div>
           </div>
-          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto">
+          {/* {image} */}
+          <motion.div 
+          variants={fadeIn("down", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          className=" lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto">
             <img
               src={Image}
               alt=""
               className="w-1/2 h-auto sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 object-cover"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
