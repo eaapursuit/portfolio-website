e7a8805842fa5f182daf3307da22cf21fac00c1c
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-10-22 15:43:10 -0400

    updated the links on my site to go to my youtube and github

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -81,10 +81,10 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="flex text-[20px] gap-x-6 max-w-max mx-auto sm:mx-0"
             >
-              <a href="#">
+              <a href="www.youtube.com/@eliTheCoder93" target="_blank" rel="noopener noreferrer">
                 <FaYoutube />
               </a>
-              <a href="#">
+              <a href="https://github.com/eaapursuit" target="_blank" rel="noopener noreferrer">
                 <FaGithub />
               </a>
             </motion.div>
