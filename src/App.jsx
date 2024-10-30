cc07a87898b1b492db2747a0bc578b0ee2565d04
eaapursuit <elishaabochie-ansah@pursuit.org>
2024-10-30 14:09:13 -0400

    made it production ready

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
         <Nav />
         <About />
         <Services />
-        <Work />
-        <Contact />
+        {/* <Work />
+        <Contact /> */}
         <div className="h-[4000px]"></div>
       </div>
     </>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -35,7 +35,7 @@ const Services = () => {
           initial='hidden'
           whileInView={'show'}
           viewport={{ once: false, amount: 0.3 }}
-          className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+          className="flex-1 lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
             <h2 className="font-primary text-[32px] text-accent mb-6 tracking-[10%] uppercase">
               What I Do.
             </h2>
